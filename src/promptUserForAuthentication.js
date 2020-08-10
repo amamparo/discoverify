73e0478f4493b3d0ab3dea700eb24cb122ba4081
amamparo <aaronmamparo@gmail.com>
2020-08-10 13:03:11 -0500

    use proper client scope to get user market

--- src/promptUserForAuthentication.js
+++ src/promptUserForAuthentication.js
@@ -4,7 +4,7 @@ export default () => {
   window.location.href = [
     'https://accounts.spotify.com/authorize?response_type=token',
     `&client_id=${SPOTIFY_CLIENT_ID}`,
-    `&scope=${encodeURIComponent('user-top-read')}`,
+    `&scope=${encodeURIComponent('user-read-private')}`,
     `&redirect_uri=${encodeURIComponent(`${window.location.origin}${CALLBACK_PATH}`)}`,
     `&show_dialog=true`
   ].join('');
